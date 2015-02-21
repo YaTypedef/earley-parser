79309d29c9941086fcca4d43fc89e0d6b3d39eb5
typedef <typedef@typedef-macbookair.roam.corp.google.com>
2015-02-21 13:43:25 +0000

    First version added

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # earley-parser
 Python implementation of Earley Parser
+
+Task: https://yadi.sk/i/2232ktO0epFrJ
+Usage: earley_parser.py [options]
+
+Options:
+  -h, --help            show this help message and exit
+  -g GRAMMAR_FILE, --grammar=GRAMMAR_FILE
+                        file with grammar
+  -d, --debug           print all states to stdout
+
+Testing:
+  test.py contains two test cases:
+    1) A test case from the task
+    2) http://en.wikipedia.org/wiki/Earley_parser#Example
+
+**PLEASE NOTE that stresstesting hasn't been performed.
---
+++ test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import unittest
+import earley_parser
+
+class SimpleGrammarTestCase(unittest.TestCase):
+    def setUp(self):
+    	self.grammar = earley_parser.Grammar([
+    		'S a S b S',
+    		'S'
+    	])
+        self.parser = earley_parser.EarleyParser()
+
+    def runTest(self):
+    	expected = [
+    		("abbab", False),
+    		("aaababbb", True),
+    		("abba", False),
+    		("ab", True),
+    		("", True)
+    	]
+    	for string, result in expected:
+    		self.assertEqual(self.parser.parse(string, self.grammar), result)
+
+class ArithmeticsGrammarTestCase(unittest.TestCase):
+    def setUp(self):
+    	self.grammar = earley_parser.Grammar([
+    		'S S p M',
+    		'S M',
+    		'M M m T',
+    		'M T',
+    		'T a',
+    		'T b',
+    		'T c',
+    		'T d'
+    	])
+        self.parser = earley_parser.EarleyParser()
+
+    def runTest(self):
+    	expected = [
+    		("apbmd", True),
+    		("apbmcpd", True),
+    		("apd", True),
+    		("a", True),
+    		("apmd", False),
+    		("dp", False),
+    		("p", False)
+    	]
+    	for string, result in expected:
+    		self.assertEqual(self.parser.parse(string, self.grammar), result)
+
+if __name__ == '__main__':
+    unittest.main()
